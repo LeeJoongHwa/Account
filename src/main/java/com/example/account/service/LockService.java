7174c4b2dd33f49dc3d7b31542fdd0b21afe748f
Joong <wndghk91@naver.com>
2023-06-23 22:52:24 +0900

    first commit

---
+++ src/main/java/com/example/account/service/LockService.java
@@ -0,0 +1,45 @@
+package com.example.account.service;
+
+import com.example.account.exception.AccountException;
+import com.example.account.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LockService {
+    private final RedissonClient redissonClient;
+
+    public void lock(String accountNumber) {
+        RLock lock = redissonClient.getLock(getLockKey(accountNumber));
+        log.debug("Trying lock for accountNumber : {}", accountNumber);
+
+        try {//waitTime : lock을 취득하는데 기다리는 시간(초) ?초간 lock 해제가 안된다면 취득이 안됨
+            //leaseTime : lock 자동으로 해제가 되는 시간(초) ?초간 아무 작업이 없다면 lock 해제
+            boolean isLock = lock.tryLock(1, 15, TimeUnit.SECONDS);
+            if (!isLock) {
+                log.error("======Lock acquisition failed=====");
+                throw new AccountException(ErrorCode.ACCOUNT_TRANSACTION_LOCK);
+            }
+        } catch (AccountException e) {
+            throw e;
+        } catch (Exception e) {
+            log.error("Redis lock failed", e);
+        }
+    }
+
+    public void unlock(String accountNumber) {
+        log.debug("Unlock for accountNumber : {} ", accountNumber);
+        redissonClient.getLock(getLockKey(accountNumber)).unlock();
+    }
+
+    private static String getLockKey(String accountNumber) {
+        return "ACLK:" + accountNumber;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account_user(id, name, create_at, update_at)
+values (1, 'Lee', now(), now());
+insert into account_user(id, name, create_at, update_at)
+values (2, 'Park', now(), now());
+insert into account_user(id, name, create_at, update_at)
+values (3, 'Kim', now(), now());
